1e00068836e74045a37dc2087de995050578b1e9
duynguyen.s2k2 <duynguyen.s2k2@gmail.com>
2023-03-04 09:12:25 +0700

    modal and service article

---
+++ services/ArticleService.php
@@ -0,0 +1,30 @@
+<?php 
+    include('../configs/database-connection.php');
+    include('../models/Article.php');
+
+    class ArticleService{
+        public function getAllArticles(){
+            $dbconn = new DBConnection();
+            $conn = $dbconn->getConnection();
+
+            $sql = "SELECT baiviet.ma_bviet, baiviet.tieude, baiviet.ten_bhat, theloai.ten_tloai, baiviet.tomtat,
+                    baiviet.noidung, tacgia.ten_tgia, baiviet.ngayviet, baiviet.hinhanh FROM baiviet 
+                    INNER JOIN theloai ON baiviet.ma_tloai = theloai.ma_tloai
+                    INNER JOIN tacgia ON baiviet.ma_tgia = tacgia.ma_tgia 
+                    ORDER BY baiviet.ma_bviet ASC";
+            $statment = $conn->query($sql);
+
+            $articles = [];
+
+            while($row = $statment->fetch()){
+                $article = new Article($row['ma_bviet'], $row['tieude'], $row['ten_bhat'], $row['ten_tloai'], $row['tomtat'],
+                                        $row['noidung'], $row['ten_tgia'], $row['ngayviet'], $row['hinhanh']);
+                array_push($articles, $article);
+            }
+            return $articles;
+            
+        }
+    }
+
+
+?>
